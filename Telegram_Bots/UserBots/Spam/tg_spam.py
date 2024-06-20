0fb72a1a3db644b5193cb5c0008f8c8fba26ec58
Sasha <69010894+ArmSasha@users.noreply.github.com>
2024-06-20 21:40:47 +0400

    Update tg_spam.py

--- Telegram_Bots/UserBots/Spam/tg_spam.py
+++ Telegram_Bots/UserBots/Spam/tg_spam.py
@@ -1,10 +1,10 @@
 from social_spam import Telegram
 import time
 tg = Telegram()
-tg.connect_user(api_id='24299549', api_hash='71dd49045fd731a379e84238a9e49b44', phone_number='89951039893')
+tg.connect_user(api_id='айди', api_hash='хеш', phone_number='номер')
 
 # print(tg.get_chats())
 
 while True:
-	tg.start_selective_spam([-4068993362, 1030874842], '123')
+	tg.start_selective_spam([айди кому, айди кому], '123')
 	time.sleep(2)
